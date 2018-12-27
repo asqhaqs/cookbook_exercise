3387869578aa0a64097f5ee576edd8746498fb73
xudong_ftd@163.com <xudong@360.net>
2018-12-27 14:43:29 +0800

    test

--- dalianfields/log_process_thread_id.py
+++ dalianfields/log_process_thread_id.py
@@ -7,7 +7,7 @@ import threading
 import datetime
 import os
 
-
+# 标准日志写法
 def log_thread_id():
     logging.warning("[Process id {0}]--[thread id {1}]--Time: {2}--[log_process_thread_id.py]--[log_thread_id]--[12]--[Warning]--FilePath: {3} \n [message: {4}]"
                     .format(os.getpid(), threading.currentThread().ident, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"), os.getcwd(), "this is a warning message"))
